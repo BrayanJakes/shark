851df751814dc2c760756c002b93c74d2aaefbe1
BrayanJakes <brianjakes22@gmail.com>
2019-05-19 09:10:43 -0400

    otros mas

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set('port', process.env.PORT || 3000);
 
 
 //middlewares
-app.use(express.static(__dirname + 'dist/appmega'));
+app.use(express.static(__dirname + '/dist/appmega'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use((req, res, next) => {
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get('*', function(req,res) {
     
-    res.sendFile(path.join(__dirname+'dist/appmega/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/appmega/index.html'));
     });
 
 
